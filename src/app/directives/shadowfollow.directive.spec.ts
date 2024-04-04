ee8930f22deff45c75f75187e05f84d6697faf2f
Leanne <sooyeunleanne@gmail.com>
2024-04-04 19:06:59 -0400

    deleted unused directive

--- src/app/directives/shadowfollow.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShadowfollowDirective } from './shadowfollow.directive';
-
-describe('ShadowfollowDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShadowfollowDirective();
-    expect(directive).toBeTruthy();
-  });
-});
