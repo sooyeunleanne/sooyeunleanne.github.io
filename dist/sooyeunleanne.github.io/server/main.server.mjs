608d6839b7606e81babc2c64882b4941290b664c
Leanne <sooyeunleanne@gmail.com>
2024-05-31 15:01:04 -0400

    updating styling

--- dist/sooyeunleanne.github.io/server/main.server.mjs
+++ dist/sooyeunleanne.github.io/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-R4BDLWXQ.mjs";import"./chunk-SF4BLIGU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2XOIGJKY.mjs";import"./chunk-SF4BLIGU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
