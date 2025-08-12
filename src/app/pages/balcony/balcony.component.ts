216aeac4d34bf7df656c373bc3f21d37983ce19f
Leanne <sooyeunleanne@gmail.com>
2025-08-11 21:45:20 -0700

    init balcony page

---
+++ src/app/pages/balcony/balcony.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balcony',
+  standalone: true,
+  imports: [],
+  templateUrl: './balcony.component.html',
+  styleUrl: './balcony.component.css'
+})
+export class BalconyComponent {
+
+}
