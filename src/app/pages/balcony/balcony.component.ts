9e7804a3eb3a5b293295d81114df5c4b3361b969
Leanne <sooyeunleanne@gmail.com>
2025-08-11 23:51:42 -0700

    link redirection for each social media icons

--- src/app/pages/balcony/balcony.component.ts
+++ src/app/pages/balcony/balcony.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './balcony.component.css'
 })
 export class BalconyComponent {
+  onInstagramClick() {
+    window.open('https://www.instagram.com/sooyeunleanne', '_blank');
+  }
 
+  onLinkedInClick() {
+    window.open('https://www.linkedin.com/in/sooyeunleanne', '_blank');
+  }
+
+  onGithubClick() {
+    window.open('https://github.com/sooyeunleanne', '_blank');
+  }
 }
