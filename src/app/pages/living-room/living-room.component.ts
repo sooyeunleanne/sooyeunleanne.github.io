559e5fcfab1b4b48aabe0acc5c55ef12b0bd17fb
Leanne <sooyeunleanne@gmail.com>
2025-08-13 08:34:25 -0700

    init living room

---
+++ src/app/pages/living-room/living-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-living-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './living-room.component.html',
+  styleUrl: './living-room.component.css'
+})
+export class LivingRoomComponent {
+
+}
