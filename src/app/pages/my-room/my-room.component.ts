c2396490e8d18dfc83cae0aea93defa32b5743d4
Leanne <sooyeunleanne@gmail.com>
2025-08-13 08:34:34 -0700

    init my-room

---
+++ src/app/pages/my-room/my-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-room.component.html',
+  styleUrl: './my-room.component.css'
+})
+export class MyRoomComponent {
+
+}
