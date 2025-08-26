6a4711485523d8fedbad5343e24b65f690711ae1
Leanne <sooyeunleanne@gmail.com>
2025-08-25 21:52:08 -0700

    added social media to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -33,4 +33,16 @@ export class HeaderComponent {
       }
     }
   }
+
+  onInstagramClick() {
+    window.open('https://www.instagram.com/sooyeunleanne', '_blank');
+  }
+
+  onLinkedInClick() {
+    window.open('https://www.linkedin.com/in/sooyeunleanne', '_blank');
+  }
+
+  onGithubClick() {
+    window.open('https://github.com/sooyeunleanne', '_blank');
+  }
 }
